7515c270799d2951b7f533bc1cbe429c16256564
mby123 <2960606748@qq.com>
2020-07-08 08:53:08 +0800

    index.vue上传

---
+++ index.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			console.log("onLoad...")
+		},
+		onPageScroll(e){
+			// console.log(e.scrollTop)
+			// if(e.scrollTop>100){
+			// 	alert("达到了100")
+			// }
+		},
+		//上拉加载
+		onReachBottom(){
+			console.log("到底了")
+		},
+		//下拉刷新
+		onPullDownRefresh(){
+			console.log("下拉刷新")
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
